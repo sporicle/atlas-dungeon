762aaad571f80976c2b921aabd9c7c7bfe3266eb
fauxfire <andyweng97@gmail.com>
2024-10-16 14:59:57 +0700

    added basic methods

--- tests/atlas-dungeon.ts
+++ tests/atlas-dungeon.ts
@@ -1,16 +1,36 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AtlasDungeon } from "../target/types/atlas_dungeon";
+import { expect } from "chai";
 
 describe("atlas-dungeon", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.AtlasDungeon as Program<AtlasDungeon>;
+  const playerState = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+      .initialize()
+      .accounts({ playerState: playerState.publicKey })
+      .signers([playerState])
+      .rpc();
+
+    const account = await program.account.playerState.fetch(playerState.publicKey);
+    expect(account.coins.toNumber()).to.equal(0);
+    expect(account.workers.toNumber()).to.equal(0);
+    expect(account.lastClickTime.toNumber()).to.equal(0);
+  });
+
+  it("Clicked", async () => {
+    const tx = await program.methods
+      .click()
+      .accounts({ playerState: playerState.publicKey, user: provider.wallet.publicKey })
+      .rpc();
+
+    const account = await program.account.playerState.fetch(playerState.publicKey);
+    expect(account.coins.toNumber()).to.equal(1);
+    expect(account.lastClickTime).to.not.equal(0);
   });
 });
